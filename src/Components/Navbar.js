d5d0f785ac3314297e0705ab60b0c598f195f658
lvuk <luka.vuk456@gmail.com>
2023-06-14 16:39:42 +0200

    nav done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,16 @@
+import { Link } from 'react-router-dom';
+import logo from '../images/logo.png';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='nav-container'>
+      <div className='nav-left'>
+        <img src={logo} alt='' />
+      </div>
+      <div className='nav-right'>
+        <Link to='cart'>Cart</Link>
+      </div>
+    </div>
+  );
 };
 export default Navbar;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
-import Navbar from '../Components/Navbar';
-import Products from '../Components/Products';
+import Navbar from '../components/Navbar';
+import Products from '../components/Products';
+import '../css/home.css';
 
 const Home = () => {
   return (
-    <div>
+    <div className='wrapper'>
       <Navbar />
       <Products />
     </div>
